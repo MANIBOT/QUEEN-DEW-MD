a93791e8ff6a03bc2cea5ef3d750f98c30d83f42
SAVIYA-MD <126868140+saviya55@users.noreply.github.com>
2024-02-23 14:04:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GR
 ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
 ADDRESSES: process.env.ADDRESSES === undefined ? '' : process.env.ADDRESSES,
 ALIVE: process.env.ALIVE === undefined ? `Hello im alive now !!` : process.env.ALIVE,
-OWNER: process.env.OWNER === undefined ? `94725881990` : process.env.OWNER,
+OWNER: process.env.OWNER === undefined ? `` : process.env.OWNER,
 PREFIX: process.env.PREFIX === undefined ? '@' : process.env.PREFIX,
-FOOTER: process.env.FOOTER=== undefined ? '‌ꜱᴀᴄʜɪʙᴏᴛ': process.env.FOOTER,
-LOGO: process.env.LOGO === undefined ? `https://i.ibb.co/x3rh1ZF/newsbot.png` : process.env.LOGO
+FOOTER: process.env.FOOTER=== undefined ? '': process.env.FOOTER,
+LOGO: process.env.LOGO === undefined ? `` : process.env.LOGO
 };
