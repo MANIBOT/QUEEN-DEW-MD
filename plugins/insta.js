5b3a01f324a50077aa82a261c33f987c40a77fc0
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-02-28 13:54:29 +0530

    Add files via upload

---
+++ plugins/insta.js
@@ -0,0 +1,33 @@
+const { cmd } = require('../command')
+
+async function Insta(match) {
+    const result = []
+                    const form = {
+                        url: match,
+                        submit: '',
+                    }
+                    const { data } = await axios(`https://downloadgram.org/`, {
+                        method: 'POST',
+                        data: form
+                    })
+                    const $ = cheerio.load(data)
+                    $('#downloadhere > a').each(function (a,b) {
+                    const url = $(b).attr('href')
+                    if (url) result.push(url)
+                })
+                return result
+    }
+
+cmd({
+        pattern: "insta",
+        desc: "download instagram post.",
+        category: "downloader",
+        filename: __filename
+    },
+    async(conn, mek, m,{from, q}) => {
+if(!q) return m.reply('Need post url.')
+let response = await Insta(q)
+for (let i=0;i<response.length;i++) {
+await conn.sendFileUrl(from, response[i], `*Downloaded Media from instagram.*`, mek)
+}
+    });
