51672972a3392b3b6778c0e34b696618e3bfb27e
SAVIYA-MD <126868140+saviya55@users.noreply.github.com>
2024-03-23 18:02:27 +0530

    Create audio.js

---
+++ plugins/audio.js
@@ -0,0 +1,34 @@
+cmd({
+        pattern: "bass",
+        desc: "adds bass in given sound",
+        category: "audio",
+        use: '<reply to any audio>',
+        react:"😎",
+    },
+    async(Void, citel) => {
+        let mime = citel.quoted.mtype
+        let set = "-af equalizer=f=54:width_type=o:width=2:g=20";
+        if (/audio/.test(mime)) {
+            citel.send(tlang().wait);
+            let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
+            let ran = citel.sender.slice(6) + (".mp3");
+            exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media);
+                if (err) return reply(err);
+                let buff = fs.readFileSync(ran);
+                Void.sendMessage(
+                    citel.chat, {
+                        audio: buff,
+                        mimetype: "audio/mpeg",
+                    }, {
+                        quoted: citel,
+                    }
+                );
+                fs.unlinkSync(ran);
+            });
+        } else
+            citel.send(
+                `Reply to the audio you want to change with*`
+            );
+    }
+)
