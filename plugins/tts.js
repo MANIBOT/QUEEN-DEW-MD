32dc8902ae7a73be4df3f7111c02e5fbbdd074df
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-03-23 13:23:06 +0530

    Create tts.js

---
+++ plugins/tts.js
@@ -0,0 +1,34 @@
+const { cmd } = require('../command')
+
+
+cmd({
+            pattern: "tts",
+            react: "💭",
+            desc: "q to speech.",
+            category: "downloader",
+            filename: __filename,
+            use: '<Hii,this is Secktor>',
+       },
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname,  isSachintha, isSavi, isSadas, isMani, isMe,isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+if(!isOwner && !isSachintha && !isSavi && !isSadas && !isMani && !isMe)return;
+    try{
+        async(mek, m, q) => {
+            if (!q) return m.reply('Please give me Sentence to change into audio.')
+            let qtts = q
+            const ttsurl = googleTTS.getAudioUrl(qtts, {
+                lang: "en",
+                slow: false,
+                host: "https://translate.google.com",
+            });
+            return mek.sendMessage(m.chat, {
+                audio: {
+                    url: ttsurl,
+                },
+                mimetype: "audio/mpeg",
+                fileName: `ttsmmek.m4a`,
+            }, {
+                quoted: m,
+            });
+        }
+
+    )
