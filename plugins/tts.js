afbf18b41a84a96a4f77d7bb811a51066a529ea2
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-03-23 11:14:37 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -18,8 +18,7 @@ if(!isOwner && !isSachintha && !isSavi && !isSadas && !isMani && !isMe)return;
                 lang: "en",
                 slow: false,
                 host: "https://translate.google.com",
-            },
-              return mek.sendMessage(m.chat, {
+            },return mek.sendMessage(m.chat, {
                 audio: {
                     url: ttsurl,
                 },
