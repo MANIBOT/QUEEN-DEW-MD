2fda66688043c5b0caf9ff2c2816b6c76ac6b6a7
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-04-13 15:50:01 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -1,4 +1,5 @@
-const { cmd } = require('../command')
+const config = require('../config');
+const { cmd, commands } = require('../command')
 const googleTTS = require("google-tts-api");
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
