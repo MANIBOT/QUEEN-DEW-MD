2155965cf6b0dc98673600475d1fa41573521c10
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-04-14 16:52:04 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,5 +1,7 @@
 const config = require('../config');
 const { cmd, commands } = require('../command')
+const fbdl = require("fbdl-core");
+const fs = require("fs");
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
 var needus = "*Please give me facebook url* !!" 
