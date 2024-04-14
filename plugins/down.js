71ab301db2ab861c48c851a671d3c65e983f6d58
Sadas Ofc <146169926+sadasofc@users.noreply.github.com>
2024-04-14 17:03:25 +0530

    Create down.js

---
+++ plugins/down.js
@@ -0,0 +1,29 @@
+const config = require('../config')
+const fg = require('api-dylux');
+const l = console.log
+const { cmd, commands } = require('../command')
+const dl = require('@bochilteam/scraper')  
+const ytdl = require('youtubedl-core');
+const fs = require('fs-extra')
+
+pattern: ".down",
+    alias: ["ytsearch"],
+    use: '.down lelena',
+    react: "🔎",
+    desc: "Search and get details from youtube.",
+    category: "search",
+    filename: __filename
+
+},
+
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+
+youtubedl.download('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  .then(info => {
+    console.log('Download complete');
+  })
+  .catch(err => {
+    console.error(err);
+  });
